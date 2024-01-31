b27e9125430ba1edf3a4967e6d010a3999d60624
jayp822 <jaydpat0822@gmail.com>
2024-01-31 09:45:11 -0500

    added plugins

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,11 +7,8 @@ vim.cmd("set spell")
 vim.g.mapleader = " "
 
 --Neotree Filesystem Navigation
-vim.keymap.set('n', '<leader>a', ':Neotree filesystem reveal left<CR>', {})
-vim.keymap.set('n', '<leader>d', ':Neotree filesystem reveal right<CR>', {})
-vim.keymap.set('n', '<leader>w', ':Neotree filesystem reveal top<CR>', {})
-vim.keymap.set('n', '<leader>s', ':Neotree filesystem reveal bottom<CR>', {})
+vim.keymap.set("n", "<leader>a", ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set("n", "<leader>d", ":Neotree filesystem reveal right<CR>", {})
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
-
